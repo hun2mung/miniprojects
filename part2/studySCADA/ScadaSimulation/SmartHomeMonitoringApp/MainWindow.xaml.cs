043de92d4308999107916a6ed114b2324c48ae20
hun2mung <ldh5444@naver.com>
2023-05-12 13:46:53 +0900

    23-05-12

--- part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/MainWindow.xaml.cs
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using SmartHomeMonitoringApp.Views;
 using MahApps.Metro.Controls.Dialogs;
 using SmartHomeMonitoringApp.Logics;
 using System.ComponentModel;
+using ControlzEx.Theming;
 
 namespace SmartHomeMonitoringApp
 {
@@ -26,6 +27,9 @@ namespace SmartHomeMonitoringApp
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        string DefaultTheme { get; set; } = "Light";    // 기본 테마
+        string DefaultAccent { get; set; } = "Cobalt";   // 기본 액센트
+
         public MainWindow()
         {
             InitializeComponent();
@@ -73,16 +77,16 @@ namespace SmartHomeMonitoringApp
                 AnimateHide = true
             };
 
-            var result = await this.ShowMessageAsync("프로그램 끝내기", "프로그램을 끝내시겠습니까?", 
+            var result = await this.ShowMessageAsync("프로그램 끝내기", "프로그램을 끝내시겠습니까?",
                                                     MessageDialogStyle.AffirmativeAndNegative, mySettings);
 
             if (result == MessageDialogResult.Negative)
             {
                 e.Cancel = true;
             }
-            else if(result == MessageDialogResult.Affirmative)
+            else if (result == MessageDialogResult.Affirmative)
             {
-                if(Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+                if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
                 {
                     Commons.MQTT_CLIENT.Disconnect();
                 }
@@ -102,7 +106,7 @@ namespace SmartHomeMonitoringApp
             var userControl = new Views.DataBaseControl();
             ActiveItem.Content = userControl;
             StsSelScreen.Content = "DataBase Monitoring";
-            
+
         }
 
         private void MnuRealTimeMon_Click(object sender, RoutedEventArgs e)
@@ -122,6 +126,63 @@ namespace SmartHomeMonitoringApp
             var about = new About();
             about.Owner = this;
             about.ShowDialog();
+        }
+
+        // 모든 테마와 액센트 전부 처리할 체크 이벤트 핸들러
+        private void MnuThemeAccent_Click(object sender, RoutedEventArgs e)
+        {
+            // 클릭되는 테마가 라이트인지 다크인지 확인. => 라이트 클릭->다크 해제 / 다크 클릭->라이트 해제
+
+
+            // 액센트도 같게
+
+            switch ((sender as MenuItem).Header)
+            {
+                case "Light":
+                    MnuLightTheme.IsChecked = true;
+                    MnuDarkTheme.IsChecked = false;
+                    break;
+                case "Dark":
+                    MnuLightTheme.IsChecked = false;
+                    MnuDarkTheme.IsChecked = true;
+                    break;
+                case "Amber":
+                    MnuAccentAmber.IsChecked = true;
+                    MnuAccentBlue.IsChecked = false;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Amber";
+                    break;
+                case "Blue":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = true;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Blue";
+
+                    break;
+
+                case "Brown":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = true;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Blue";
+
+                    break;
+
+                case "Cobalt":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = false;
+                    MnuAccentBrown.IsChecked = true;
+                    MnuAccentCobalt.IsChecked = true;
+                    DefaultAccent = "Cobalt";
+
+                    break;
+            }
+
+
+            ThemeManager.Current.ChangeTheme(this, "Dark.Amber");
         }
     }
 }
